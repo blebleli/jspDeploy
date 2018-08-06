60a77552001c43ed9801c4b50a3e2631f887863a
“blebleli <hsj128jhs@gmail.com>
2018-08-06 15:18:52 +0900

    y

--- src/main/java/kr/or/ddit/servlet/SumCalulation.java
+++ src/main/java/kr/or/ddit/servlet/SumCalulation.java
@@ -1,6 +1,6 @@
 package kr.or.ddit.servlet;
 
-import java.io.IOException;
+import java.io.IOException; 
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
 /**
  * Servlet implementation class SumCalulation
